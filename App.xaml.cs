f9e55f4985c55f3b55476c2e3cf81547ba0d8d97
DJ Nastya <61619282+jirootik@users.noreply.github.com>
2023-07-11 21:49:53 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,57 @@
+﻿using DiascanPraktika.DatabaseData;
+using DiascanPraktika.Services;
+using DiascanPraktika.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Windows;
+
+namespace DiascanPraktika
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+
+        private IHost __Host;
+
+        public IHost Host
+        {
+
+            get
+            {
+                Program program = new Program();
+                return __Host
+            = program.CreateHostBuilder(Environment.GetCommandLineArgs(), this).Build();
+            }
+        }
+
+        public IServiceProvider Services => Host.Services;
+
+        internal void ConfigureServices(HostBuilderContext host, IServiceCollection services) => services
+           .AddDatabase(host.Configuration.GetSection("Database"))
+           .AddServices()
+           .AddViewModels()
+        ;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            var host = Host;
+
+            using (var scope = Services.CreateScope())
+                scope.ServiceProvider.GetRequiredService<DbInitializer>().Initialize();
+            base.OnStartup(e);
+            host.Start();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using var host = Host;
+            base.OnExit(e);
+            await host.StopAsync();
+        }
+    }
+}
+
