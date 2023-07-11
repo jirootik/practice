e4cd673a113f99ea0861c04a3d0644a7741c1923
DJ Nastya <61619282+jirootik@users.noreply.github.com>
2023-07-11 21:47:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+
+
+namespace DiascanPraktika
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        public IHostBuilder CreateHostBuilder(string[] args, App app)
+        {
+            return Host.
+            CreateDefaultBuilder(args)
+            .ConfigureServices(app.ConfigureServices);
+        }
+    }
+
+
+}
